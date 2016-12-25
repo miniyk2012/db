c943c4ed8f47f77e4236b6a4639ef6c15f617752
杨恺 <yangk@ersoft.cn>
2016-12-25 10:20:17 +0800

    基本sql操作，连表搜索，删除，更新操作

--- f1a3b402-137b-423a-988d-7c5ed65dbcb2/console_3.sql
+++ f1a3b402-137b-423a-988d-7c5ed65dbcb2/console_3.sql
@@ -81,5 +81,95 @@ CREATE TABLE `song_list` (
 
 show create table song_list;
 
+INSERT INTO song_list VALUES ('1', 'Good Lovin\' Gone Bad', 'Bad Company', '0', '0', 'Straight Shooter', '453', '0');
+INSERT INTO song_list VALUES ('2', 'Weep No More', 'Bad Company', '0', '0', 'Straight Shooter', '280', '0');
+INSERT INTO song_list VALUES ('3', 'Shooting Star', 'Bad Company', '0', '0', 'Straight Shooter', '530', '0');
+INSERT INTO song_list VALUES ('4', '大象', '李志', '0', '0', '1701', '560', '0');
+INSERT INTO song_list VALUES ('5', '定西', '李志', '0', '0', '1701', '1023', '0');
+INSERT INTO song_list VALUES ('6', '红雪莲', '洪启', '0', '0', '红雪莲', '220', '0');
+INSERT INTO song_list VALUES ('7', '风柜来的人', '李宗盛', '0', '0', '作品李宗盛', '566', '0');
+
+SELECT * from song_list;
+
+-- ----------------------------
+-- Table structure for `stu`
+-- ----------------------------
+DROP TABLE IF EXISTS `stu`;
+CREATE TABLE `stu` (
+  `id` int(10) NOT NULL DEFAULT '0',
+  `name` varchar(20) DEFAULT NULL,
+  `age` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for `tbl_proc_test`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_proc_test`;
+CREATE TABLE `tbl_proc_test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `num` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+  -- ----------------------------
+-- Records of tbl_proc_test
+-- ----------------------------
+INSERT INTO tbl_proc_test VALUES ('11', '1');
+INSERT INTO tbl_proc_test VALUES ('12', '2');
+INSERT INTO tbl_proc_test VALUES ('13', '6');
+INSERT INTO tbl_proc_test VALUES ('14', '24');
+INSERT INTO tbl_proc_test VALUES ('15', '120');
+INSERT INTO tbl_proc_test VALUES ('16', '720');
+INSERT INTO tbl_proc_test VALUES ('17', '5040');
+INSERT INTO tbl_proc_test VALUES ('18', '40320');
+INSERT INTO tbl_proc_test VALUES ('19', '362880');
+INSERT INTO tbl_proc_test VALUES ('20', '3628800');
+INSERT INTO tbl_proc_test VALUES ('21', '1');
+INSERT INTO tbl_proc_test VALUES ('22', '2');
+INSERT INTO tbl_proc_test VALUES ('23', '6');
+INSERT INTO tbl_proc_test VALUES ('24', '24');
+INSERT INTO tbl_proc_test VALUES ('25', '1');
+INSERT INTO tbl_proc_test VALUES ('26', '2');
+INSERT INTO tbl_proc_test VALUES ('27', '6');
+INSERT INTO tbl_proc_test VALUES ('28', '24');
+INSERT INTO tbl_proc_test VALUES ('29', '120');
+
+SELECT * from tbl_proc_test;
+
+
+DROP TABLE IF EXISTS `A`;
+create table A (
+  id int(11) not null AUTO_INCREMENT,
+  num int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+insert into A (num) values (20), (30), (40), (50);
+SELECT * from A;
+
+INSERT INTO A VALUES (2,99) ON DUPLICATE KEY UPDATE NUM=99;
+SELECT * FROM A;
+
+DROP TABLE IF EXISTS `B`;
+create table B (
+  id int(11) not null AUTO_INCREMENT,
+  num int(11) DEFAULT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+insert into B (num, name) values (71, 'yangkai'), (72, 'zhoumin');
+SELECT *
+FROM B;
+
+UPDATE A,B set A.num=B.num where A.id=B.id;
+
+# 连表delete
+DELETE A from A,B where A.id=B.id and B.name='zhoumin';
+
+
+
+
+
 
 
